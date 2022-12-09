cc84bed46c8211fa79d0e8a827876848020c4c5e
김지호 <crsn1111@gmail.com>
2022-12-09 18:36:47 +0900

    api 수정

--- src/core/api/axios.js
+++ src/core/api/axios.js
@@ -4,7 +4,11 @@ export const instance = axios.create({
   baseURL: "http://13.124.28.246",
 });
 
-instance.interceptors.request.use((config) => {
+export const baseURL = axios.create({
+  baseURL: "http://13.124.28.246",
+});
+
+baseURL.interceptors.request.use((config) => {
   if (config.headers === undefined) return;
   const token = localStorage.getItem("id");
   config.headers["authorization"] = `Bearer ${token}`;
